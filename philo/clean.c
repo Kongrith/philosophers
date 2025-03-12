18adf7cb7041a2c0c6673749aec6a873839b13e1
Kongrith <capuchino_cartoon@hotmail.com>
2025-03-12 16:34:20 +0700

    reinit clean.c

---
+++ philo/clean.c
@@ -0,0 +1,82 @@
+
+#include "philo.h"
+
+// var->philo = malloc(sizeof(t_philo) * var->num_of_philo);
+// 	var->monitor = malloc(sizeof(t_monitor));
+// 	var->forks = malloc(sizeof(pthread_mutex_t) * var->num_of_philo);
+
+/*
+typedef struct s_fork
+{
+    t_mtx fork;
+    int fork_id;
+} t_fork;
+
+typedef struct s_philo
+{
+    int id;
+    long meals_counter;
+    bool full;
+    long last_meal_timestamp; // time passed from last meal
+    t_fork *first_fork;
+    t_fork *second_fork;
+    pthread_t thread_id; // philo is a thread
+    t_mtx philo_mutex;
+    t_table *table;
+} t_philo;
+*/
+// void clean(t_table *table)
+// {
+// 	t_philo *philo;
+// 	int i;
+
+// 	i = -1;
+// 	while (++i < table->philo_nbr)
+// 	{
+// 		philo = table->philos + 1;
+// 		safe_mutex_handle(&philo->philo_mutex, DESTROY);
+// 	}
+// 	safe_mutex_handle(&table->write_mutex, DESTROY);
+// 	safe_mutex_handle(&table->table_mutex, DESTROY);
+// 	free(table->forks);
+// 	free(table->philos);
+// }
+
+static void free_philosophers(t_var *var)
+{
+    int i;
+
+    i = 0;
+    while (i < var->num_of_philo)
+    {
+        //     // if (var->philo[i] != NULL)
+        //     //     free(var->philo[i]);
+        // printf("ID:%d\n", var->philo[i].id);
+        i++;
+    }
+}
+
+static void destroy_mutex(t_var *var)
+{
+    int i;
+
+    i = 0;
+    while (i < var->num_of_philo)
+    {
+        pthread_mutex_unlock(&var->forks[i]);
+        pthread_mutex_destroy(&var->forks[i]);
+        i++;
+    }
+}
+
+void clean(t_var *var, int level)
+{
+    if (level == 2)
+    {
+        free_philosophers(var);
+        destroy_mutex(var);
+    }
+    free(var->forks);
+    free(var->philo);
+    free(var->monitor);
+}
