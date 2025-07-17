a870d494079e2ae19bfa54da1d86b969c3cca65a
Kongrith <capuchino_cartoon@hotmail.com>
2025-07-17 17:30:43 +0700

    norm 100%

--- philo/thread_handler.c
+++ philo/thread_handler.c
@@ -6,88 +6,59 @@
 /*   By: kkomasat <kkomasat@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 15:19:42 by kkomasat          #+#    #+#             */
-/*   Updated: 2025/07/17 16:04:39 by kkomasat         ###   ########.fr       */
+/*   Updated: 2025/07/17 17:28:05 by kkomasat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void lone_philo(t_philo *philo)
+void	lone_philo(t_philo *philo)
 {
 	wait_all_threads(philo->var);
-	set_timestamp(&philo->lastmeal_mutex, &philo->last_meal_timestamp,
-				  timestamp_in_ms());
+	set_timestamp(&philo->lastmeal_mutex, &philo->last_meal_timestamp, \
+timestamp_in_ms());
 	write_status(TAKE_FIRST_FORK, philo, DEBUG_MODE);
 	while (1)
 	{
-		if (timestamp_in_ms() - get_timestamp(&philo->lastmeal_mutex,
-											  philo->last_meal_timestamp) >
-			get_long(&philo->var->time2die_mutex,
-					 philo->var->time_to_die))
+		if (timestamp_in_ms() - get_timestamp(&philo->lastmeal_mutex, \
+philo->last_meal_timestamp) > get_long(&philo->var->time2die_mutex, \
+philo->var->time_to_die))
 		{
-			break;
+			break ;
 		}
 	}
 }
 
-int stoping_criteria(t_philo *philo)
+int	stoping_criteria(t_philo *philo)
 {
-	if (get_int(&philo->remaining_meals_mutex, philo->remaining_meals) &&
-		(!get_int(&philo->var->isdead_mutex, philo->var->is_dead)))
+	if (get_int(&philo->remaining_meals_mutex, philo->remaining_meals) && \
+(!get_int(&philo->var->isdead_mutex, philo->var->is_dead)))
 		return (1);
 	else
 		return (0);
 }
 
-void even_odd_approach(t_philo *philo)
+void	even_odd_approach(t_philo *philo)
 {
 	wait_all_threads(philo->var);
-	set_timestamp(&philo->lastmeal_mutex, &philo->last_meal_timestamp, timestamp_in_ms());
+	set_timestamp(&philo->lastmeal_mutex, &philo->last_meal_timestamp, \
+timestamp_in_ms());
 	start_scheme(philo);
-	// if (get_int(&philo->id_mutex, philo->id) % 2 == 0)
-	// 	usleep(get_long(&philo->var->time2eat_mutex, philo->var->time_to_eat) * 1000 * 0.5);
-	// else if (get_int(&philo->id_mutex, philo->id) == philo->var->num_of_philo)
-	// {
-	// 	usleep(get_long(&philo->var->time2eat_mutex, philo->var->time_to_eat) * 1000 * 0.6);
-	// }
-	// while (get_int(&philo->remaining_meals_mutex, philo->remaining_meals) && (!get_int(&philo->var->isdead_mutex, philo->var->is_dead)))
 	while (stoping_criteria(philo))
 	{
 		if (get_int(&philo->var->isdead_mutex, philo->var->is_dead))
-			break;
+			break ;
 		if (eat_event(philo) < 0)
-			break;
-		// pthread_mutex_lock(&philo->var->forks[philo->first_fork]);
-		// if (get_int(&philo->var->isdead_mutex, philo->var->is_dead))
-		// {
-		// 	pthread_mutex_unlock(&philo->var->forks[philo->first_fork]);
-		// 	break;
-		// }
-		// write_status(TAKE_FIRST_FORK, philo, DEBUG_MODE);
-		// pthread_mutex_lock(&philo->var->forks[philo->second_fork]);
-		// if (get_int(&philo->var->isdead_mutex, philo->var->is_dead))
-		// {
-		// 	pthread_mutex_unlock(&philo->var->forks[philo->first_fork]);
-		// 	pthread_mutex_unlock(&philo->var->forks[philo->second_fork]);
-		// 	break;
-		// }
-		// write_status(TAKE_SECOND_FORK, philo, DEBUG_MODE);
-		// write_status(EATING, philo, DEBUG_MODE);
-		// set_timestamp(&philo->lastmeal_mutex, &philo->last_meal_timestamp, timestamp_in_ms());
-		// usleep(get_long(&philo->var->time2eat_mutex, philo->var->time_to_eat) * 1000);
-		// pthread_mutex_unlock(&philo->var->forks[philo->first_fork]);
-		// pthread_mutex_unlock(&philo->var->forks[philo->second_fork]);
+			break ;
 		if (get_int(&philo->var->isdead_mutex, philo->var->is_dead))
-			break;
+			break ;
 		if (sleep_event(philo) < 0)
-			break;
-		// write_status(SLEEPING, philo, DEBUG_MODE);
-		// usleep(get_long(&philo->var->time2sleep_mutex, philo->var->time_to_sleep) * 1000);
+			break ;
 		if (get_int(&philo->var->isdead_mutex, philo->var->is_dead))
-			break;
+			break ;
 		if (think_event(philo) < 0)
-			break;
-		// write_status(THINKING, philo, DEBUG_MODE);
-		set_int(&philo->remaining_meals_mutex, &philo->remaining_meals, philo->remaining_meals - 1);
+			break ;
+		set_int(&philo->remaining_meals_mutex, &philo->remaining_meals, \
+philo->remaining_meals - 1);
 	}
 }
--- philo/thread_handler_utils.c
+++ philo/thread_handler_utils.c
@@ -6,60 +6,65 @@
 /*   By: kkomasat <kkomasat@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 16:34:44 by kkomasat          #+#    #+#             */
-/*   Updated: 2025/07/17 16:34:45 by kkomasat         ###   ########.fr       */
+/*   Updated: 2025/07/17 17:25:23 by kkomasat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void start_scheme(t_philo *philo)
+void	start_scheme(t_philo *philo)
 {
-    if (get_int(&philo->id_mutex, philo->id) % 2 == 0)
-        usleep(get_long(&philo->var->time2eat_mutex, philo->var->time_to_eat) * 1000 * 0.5);
-    else if (get_int(&philo->id_mutex, philo->id) == philo->var->num_of_philo)
-        usleep(get_long(&philo->var->time2eat_mutex, philo->var->time_to_eat) * 1000 * 0.6);
+	if (get_int(&philo->id_mutex, philo->id) % 2 == 0)
+		usleep(get_long(&philo->var->time2eat_mutex, philo->var->time_to_eat) \
+* 1000 * 0.5);
+	else if (get_int(&philo->id_mutex, philo->id) == philo->var->num_of_philo)
+		usleep(get_long(&philo->var->time2eat_mutex, philo->var->time_to_eat) \
+* 1000 * 0.6);
 }
 
-void wait_all_threads(t_var *var)
+void	wait_all_threads(t_var *var)
 {
-    while (!get_int(&var->allready_mutex, var->all_threads_ready))
-        ;
+	while (!get_int(&var->allready_mutex, var->all_threads_ready))
+		;
 }
 
-int eat_event(t_philo *philo)
+int	eat_event(t_philo *philo)
 {
-    pthread_mutex_lock(&philo->var->forks[philo->first_fork]);
-    if (get_int(&philo->var->isdead_mutex, philo->var->is_dead))
-    {
-        pthread_mutex_unlock(&philo->var->forks[philo->first_fork]);
-        return (-1);
-    }
-    write_status(TAKE_FIRST_FORK, philo, DEBUG_MODE);
-    pthread_mutex_lock(&philo->var->forks[philo->second_fork]);
-    if (get_int(&philo->var->isdead_mutex, philo->var->is_dead))
-    {
-        pthread_mutex_unlock(&philo->var->forks[philo->first_fork]);
-        pthread_mutex_unlock(&philo->var->forks[philo->second_fork]);
-        return (-1);
-    }
-    write_status(TAKE_SECOND_FORK, philo, DEBUG_MODE);
-    write_status(EATING, philo, DEBUG_MODE);
-    set_timestamp(&philo->lastmeal_mutex, &philo->last_meal_timestamp, timestamp_in_ms());
-    usleep(get_long(&philo->var->time2eat_mutex, philo->var->time_to_eat) * 1000);
-    pthread_mutex_unlock(&philo->var->forks[philo->first_fork]);
-    pthread_mutex_unlock(&philo->var->forks[philo->second_fork]);
-    return (0);
+	pthread_mutex_lock(&philo->var->forks[philo->first_fork]);
+	if (get_int(&philo->var->isdead_mutex, philo->var->is_dead))
+	{
+		pthread_mutex_unlock(&philo->var->forks[philo->first_fork]);
+		return (-1);
+	}
+	write_status(TAKE_FIRST_FORK, philo, DEBUG_MODE);
+	pthread_mutex_lock(&philo->var->forks[philo->second_fork]);
+	if (get_int(&philo->var->isdead_mutex, philo->var->is_dead))
+	{
+		pthread_mutex_unlock(&philo->var->forks[philo->first_fork]);
+		pthread_mutex_unlock(&philo->var->forks[philo->second_fork]);
+		return (-1);
+	}
+	write_status(TAKE_SECOND_FORK, philo, DEBUG_MODE);
+	write_status(EATING, philo, DEBUG_MODE);
+	set_timestamp(&philo->lastmeal_mutex, &philo->last_meal_timestamp, \
+timestamp_in_ms());
+	usleep(get_long(&philo->var->time2eat_mutex, philo->var->time_to_eat) \
+* 1000);
+	pthread_mutex_unlock(&philo->var->forks[philo->first_fork]);
+	pthread_mutex_unlock(&philo->var->forks[philo->second_fork]);
+	return (0);
 }
 
-int sleep_event(t_philo *philo)
+int	sleep_event(t_philo *philo)
 {
-    write_status(SLEEPING, philo, DEBUG_MODE);
-    usleep(get_long(&philo->var->time2sleep_mutex, philo->var->time_to_sleep) * 1000);
-    return (0);
+	write_status(SLEEPING, philo, DEBUG_MODE);
+	usleep(get_long(&philo->var->time2sleep_mutex, philo->var->time_to_sleep) \
+* 1000);
+	return (0);
 }
 
-int think_event(t_philo *philo)
+int	think_event(t_philo *philo)
 {
-    write_status(THINKING, philo, DEBUG_MODE);
-    return (0);
+	write_status(THINKING, philo, DEBUG_MODE);
+	return (0);
 }
